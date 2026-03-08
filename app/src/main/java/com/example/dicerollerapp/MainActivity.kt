7901203bbccad0bdbf0b0b4adcc340ee2a4d1b6b
MoamenAbdelrhman <moamen816@outlook.com>
2026-03-09 00:07:39 +0200

    implement dice roll logic

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.dicerollerapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,37 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val dice1: ImageView = findViewById(R.id.dice1_iv)
+        val dice2: ImageView = findViewById(R.id.dice2_iv)
+        val rollBtn: Button = findViewById(R.id.roll_btn)
+
+        rollBtn.setOnClickListener {
+
+            val randomNum1 = (1..6).random()
+            val randomNum2 = (1..6).random()
+
+            val pic1 = when(randomNum1){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            val pic2 = when(randomNum2){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            dice1.setImageResource(pic1)
+            dice2.setImageResource(pic2)
+        }
+
     }
 }
